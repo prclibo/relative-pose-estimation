c318ac1e909537d253b867ff04b4e5478989eb89
Bo Li <prclibo@gmail.com>
2013-02-25 09:17:22 +0100

    more code on vcharge test

--- test/perf_test_vcharge2.cpp
+++ test/perf_test_vcharge2.cpp
@@ -43,7 +43,8 @@ int main()
 
     FrameSegment fs0 = sg.frameSegments(ci).front(); 
 
-    Mat pose_4pt = Mat::eye(4, 4, CV_64F); 
+    Mat pose_4pt_odo = Mat::eye(4, 4, CV_64F); 
+    Mat pose_4pt_gps = Mat::eye(4, 4, CV_64F); 
     Mat pose_5pt = Mat::eye(4, 4, CV_64F); 
     Mat pose_1pt = Mat::eye(4, 4, CV_64F); 
 
@@ -59,18 +60,18 @@ int main()
             if (s == 0 && i == 0) continue; 
 
             Matrix4d pose_gps_prev, pose_odo_prev; 
-            double angle; 
+            double angle_odo; 
             if (s > 0 && i == 0)
             {
                 pose_gps_prev = sg.frameSegments(ci).at(s - 1).back()->gps_ins()->pose();  
                 pose_odo_prev = sg.frameSegments(ci).at(s - 1).back()->odometer()->pose(); 
-                angle = fs.at(i)->odometer()->yaw() - sg.frameSegments(ci).at(s - 1).back()->odometer()->yaw(); 
+                angle_odo = fs.at(i)->odometer()->yaw() - sg.frameSegments(ci).at(s - 1).back()->odometer()->yaw(); 
             }
             else
             {
                 pose_gps_prev = fs.at(i - 1)->gps_ins()->pose();  
                 pose_odo_prev = fs.at(i - 1)->odometer()->pose(); 
-                angle = fs.at(i)->odometer()->yaw() - fs.at(i - 1)->odometer()->yaw(); 
+                angle_odo = fs.at(i)->odometer()->yaw() - fs.at(i - 1)->odometer()->yaw(); 
             }
 
             Matrix4d pose_gps_init = fs0.at(0)->gps_ins()->pose(); 
@@ -100,6 +101,7 @@ int main()
             Matrix4d H_cfg_rel = H_cfg * H_cfg_prev.inverse(); 
             Matrix4d H_cfo_rel = H_cfo * H_cfo_prev.inverse(); 
             
+            
             Mat R_cfg_cv, r_cfg_cv, t_cfg_cv, R_cfo_cv, r_cfo_cv; 
             Matrix3d R_cfg = H_cfg_rel.block<3, 3>(0, 0); 
             Matrix3d R_cfo = H_cfo_rel.block<3, 3>(0, 0); 
@@ -111,6 +113,8 @@ int main()
             normalize(t_cfg_cv, t_cfg_cv); 
             Rodrigues(R_cfg_cv, r_cfg_cv); 
             Rodrigues(R_cfo_cv, r_cfo_cv); 
+
+            double angle_gps = norm(r_cfg_cv) * (r_cfg_cv.at<double>(1) < 0 ? 1 : -1); 
     
             Vector3d C_cfo = H_cfo.inverse().block<3, 1>(0, 3); 
             Vector3d C_cfg = H_cfg.inverse().block<3, 1>(0, 3); 
@@ -152,20 +156,36 @@ int main()
     
             
             /*******************************************************/
-            std::vector<Mat> rvecs_4pt, tvecs_4pt; 
-            findPose4pt_numerical(points1, points2, angle, 300, Point2d(640, 400), rvecs_4pt, tvecs_4pt, CV_RANSAC, 0.999, 0.5, mask); 
+            std::vector<Mat> rvecs_4pt_odo, tvecs_4pt_odo; 
+            findPose4pt_numerical(points1, points2, angle_odo, 300, Point2d(640, 400), rvecs_4pt_odo, tvecs_4pt_odo, CV_RANSAC, 0.99, 1, mask); 
     //        std::cout << countNonZero(mask) << " / " << mask.total() << "   "; 
-            Mat R_4pt, t_4pt; 
-            if (angle * rvecs_4pt.front().at<double>(1) > 0)
-                Rodrigues(rvecs_4pt.front(), R_4pt); 
+            Mat R_4pt_odo, t_4pt_odo; 
+            if (angle_odo * rvecs_4pt_odo.front().at<double>(1) > 0)
+                Rodrigues(rvecs_4pt_odo.front(), R_4pt_odo); 
             else 
-                Rodrigues(-rvecs_4pt.front(), R_4pt); 
+                Rodrigues(-rvecs_4pt_odo.front(), R_4pt_odo); 
             
 
-            if (norm(-tvecs_4pt.front(), t_cfg_cv) < norm(tvecs_4pt.front(), t_cfg_cv))
-                t_4pt = -tvecs_4pt.front() * 1.0; 
+            if (norm(-tvecs_4pt_odo.front(), t_cfg_cv) < norm(tvecs_4pt_odo.front(), t_cfg_cv))
+                t_4pt_odo = -tvecs_4pt_odo.front() * 1.0; 
             else
-                t_4pt = tvecs_4pt.front() * 1.0; 
+                t_4pt_odo = tvecs_4pt_odo.front() * 1.0; 
+    
+            /*******************************************************/
+            std::vector<Mat> rvecs_4pt_gps, tvecs_4pt_gps; 
+            findPose4pt_numerical(points1, points2, angle_gps, 300, Point2d(640, 400), rvecs_4pt_gps, tvecs_4pt_gps, CV_RANSAC, 0.99, 1, mask); 
+    //        std::cout << countNonZero(mask) << " / " << mask.total() << "   "; 
+            Mat R_4pt_gps, t_4pt_gps; 
+            if (angle_gps * rvecs_4pt_gps.front().at<double>(1) > 0)
+                Rodrigues(rvecs_4pt_gps.front(), R_4pt_gps); 
+            else 
+                Rodrigues(-rvecs_4pt_gps.front(), R_4pt_gps); 
+            
+
+            if (norm(-tvecs_4pt_gps.front(), t_cfg_cv) < norm(tvecs_4pt_gps.front(), t_cfg_cv))
+                t_4pt_gps = -tvecs_4pt_gps.front() * 1.0; 
+            else
+                t_4pt_gps = tvecs_4pt_gps.front() * 1.0; 
     
             /*******************************************************/
             Mat rvecs_1pt, tvecs_1pt; 
@@ -179,42 +199,51 @@ int main()
     
     
             /*******************************************************/
-            t_4pt *= length; 
+            t_4pt_odo *= length; 
+            t_4pt_gps *= length; 
             t_5pt *= length; 
             t_1pt *= length; 
     
-            Mat H_4pt = Mat::eye(4, 4, CV_64F); 
+            Mat H_4pt_odo = Mat::eye(4, 4, CV_64F); 
+            Mat H_4pt_gps = Mat::eye(4, 4, CV_64F); 
             Mat H_5pt = Mat::eye(4, 4, CV_64F); 
             Mat H_1pt = Mat::eye(4, 4, CV_64F); 
     
             H_5pt(Range(0, 3), Range(0, 3)) = R_5pt * 1.0; 
             H_5pt(Range(0, 3), Range(3, 4)) = t_5pt * 1.0; 
     
-            H_4pt(Range(0, 3), Range(0, 3)) = R_4pt * 1.0; 
-            H_4pt(Range(0, 3), Range(3, 4)) = t_4pt * 1.0; 
+            H_4pt_odo(Range(0, 3), Range(0, 3)) = R_4pt_odo * 1.0; 
+            H_4pt_odo(Range(0, 3), Range(3, 4)) = t_4pt_odo * 1.0; 
+    
+            H_4pt_gps(Range(0, 3), Range(0, 3)) = R_4pt_gps * 1.0; 
+            H_4pt_gps(Range(0, 3), Range(3, 4)) = t_4pt_gps * 1.0; 
     
             H_1pt(Range(0, 3), Range(0, 3)) = R_1pt * 1.0; 
             H_1pt(Range(0, 3), Range(3, 4)) = t_1pt * 1.0; 
     
             pose_5pt = H_5pt * pose_5pt; 
-            pose_4pt = H_4pt * pose_4pt; 
+            pose_4pt_odo = H_4pt_odo * pose_4pt_odo; 
+            pose_4pt_gps = H_4pt_gps * pose_4pt_gps; 
             pose_1pt = H_1pt * pose_1pt; 
             
             if (n == start)
             {
-                eigen2cv(H_cfo, pose_4pt); 
+                eigen2cv(H_cfo, pose_4pt_odo); 
+                eigen2cv(H_cfo, pose_4pt_gps); 
                 eigen2cv(H_cfo, pose_5pt); 
                 eigen2cv(H_cfo, pose_1pt); 
             }        
     
     
             Mat C_5pt = Mat(pose_5pt.inv())(Range(0, 3), Range(3, 4)) * 1.0; 
-            Mat C_4pt = Mat(pose_4pt.inv())(Range(0, 3), Range(3, 4)) * 1.0; 
+            Mat C_4pt_odo = Mat(pose_4pt_odo.inv())(Range(0, 3), Range(3, 4)) * 1.0; 
+            Mat C_4pt_gps = Mat(pose_4pt_gps.inv())(Range(0, 3), Range(3, 4)) * 1.0; 
             Mat C_1pt = Mat(pose_1pt.inv())(Range(0, 3), Range(3, 4)) * 1.0; 
     
             std::cout << "c_cfo(" << n + 1 - start << ", :) = [" << C_cfo.transpose() << "]; " << std::endl; 
             std::cout << "c_cfg(" << n + 1 - start << ", :) = [" << C_cfg.transpose() << "]; " << std::endl; 
-            std::cout << "c_4pt(" << n + 1 - start << ", :) = " << C_4pt.t() << "; " << std::endl; 
+            std::cout << "c_4pt_odo(" << n + 1 - start << ", :) = " << C_4pt_odo.t() << "; " << std::endl; 
+            std::cout << "c_4pt_gps(" << n + 1 - start << ", :) = " << C_4pt_gps.t() << "; " << std::endl; 
             std::cout << "c_5pt(" << n + 1 - start << ", :) = " << C_5pt.t() << "; " << std::endl; 
             std::cout << "c_1pt(" << n + 1 - start << ", :) = " << C_1pt.t() << "; " << std::endl; 
             n++; 
@@ -224,11 +253,12 @@ int main()
     std::cout << "plot(c_cfg(:, 3), -c_cfg(:, 1), '-', 'color', [1, 1, 1] * 0.7, 'linewidth', 10, 'markersize', 2), hold on" << std::endl; 
     std::cout << "plot(c_cfo(:, 3), c_cfo(:, 1), '-', 'color', [1, 1, 1] * 0.4, 'linewidth', 5, 'markersize', 2), hold on" << std::endl; 
     std::cout << "plot(c_1pt(:, 3), c_1pt(:, 1), '-.g', 'linewidth', 3, 'markersize', 2), hold on" << std::endl; 
-    std::cout << "plot(c_4pt(:, 3), c_4pt(:, 1), '-b', 'linewidth', 3, 'markersize', 2), hold on" << std::endl; 
+    std::cout << "plot(c_4pt_odo(:, 3), c_4pt_odo(:, 1), '-b', 'linewidth', 3, 'markersize', 2), hold on" << std::endl; 
+    std::cout << "plot(c_4pt_gps(:, 3), c_4pt_gps(:, 1), '-m', 'linewidth', 3, 'markersize', 2), hold on" << std::endl; 
     std::cout << "plot(c_5pt(:, 3), c_5pt(:, 1), '--r', 'linewidth', 3, 'markersize', 2), hold on" << std::endl; 
     std::cout << "axis equal" << std::endl; 
     std::cout << "set(h, 'Position', [0, 0, 550, 400])" << std::endl; 
-    std::cout << "legend('GPS', 'Odo', '1-pt', '4-pt', '5-pt', 'Location', 'Northwest')" << std::endl; 
+    std::cout << "legend('GPS', 'Odo', '1-pt', '4-pt Odo', '4-pt GPS', '5-pt', 'Location', 'Northwest')" << std::endl; 
     std::cout << "text(c_cfo(1, 3) - 1.5, c_cfo(1, 1) -0.5, 'Start point')" << std::endl; 
     std::cout << "xlabel('(m)')" << std::endl; 
     std::cout << "ylabel('(m)')" << std::endl; 
